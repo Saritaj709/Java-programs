3047c62f244d4ce23e306f1776aacc147bd2b410
Sarita <saritaj709@gmail.com>
2018-05-16 18:37:01 +0530

    first commit

---
+++ Power2.java
@@ -0,0 +1,26 @@
+/*4. Power of 2
+a. Desc ­> This program takes a command­line argument N and prints a table of the
+powers of 2 that are less than or equal to 2^N.
+b. I/P ­> The Power Value N. Only works if 0 <= N < 31 since 2^31 overflows an int
+c. Logic ­> repeat until i equals N.
+d. O/P ­> Print the year is a Leap Year or not. */
+
+class Power2
+{
+public static void main(String[] args)
+{
+int N;
+N=Integer.parseInt(args[0]);
+System.out.println("enter N " +args[0]);
+if(N<0 || N>30)
+System.out.print("invalid");
+else
+{
+for(int i=0;i<=N;i++)
+{
+System.out.println("2^ "+i + "=" + Math.pow(2,+i)); 
+}
+}
+}
+}
+
